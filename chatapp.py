24a13a1421953c831264ac2bc1fc13d419fe4210
Ben <benben.goren@gmail.com>
2025-11-19 12:37:54 +0200

    "add room1 and room3 directories, update chatapp.py"

--- chatapp.py
+++ chatapp.py
@@ -13,6 +13,12 @@ def serve_room(room):
     return send_file("index.html")
 
 
+@app.route('/api/chat/<room>')
+def get_chat(room):
+    # Path to the chat file for this room
+    chat_file = f'chats/{room}.txt'
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
